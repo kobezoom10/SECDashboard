d4bf115b29d98f034087bd6fcbe405928267a53c
kobezoom10 <jwaldek13@yahoo.com>
2026-03-12 13:41:32 -0400

    Update admin.js

--- api/admin.js
+++ api/admin.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   if (req.method !== "POST") return res.status(405).json({ error: "Method not allowed" });
   try {
-    const response = await fetch("https://api.sec-api.io/administrative-proceedings", {
+    const response = await fetch("https://api.sec-api.io/sec-administrative-proceedings", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
