6bff41818c7e2c61f3a5967a6b038ef310393b31
kobezoom10 <jwaldek13@yahoo.com>
2026-03-12 13:22:30 -0400

    Update litigation.js

--- api/litigation.js
+++ api/litigation.js
@@ -1,4 +1,5 @@
-import { QueryApi } from "sec-api";
+import pkg from "sec-api";
+const { QueryApi } = pkg;
 
 export default async function handler(req, res) {
   if (req.method !== "POST") return res.status(405).json({ error: "Method not allowed" });
